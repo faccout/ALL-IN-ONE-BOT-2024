6858330aff88ceb823dcbc41b7fb0e564ef71c9f
faccout <faccout41@gmail.com>
2024-12-16 16:04:24 +0300

    Update mpause.js

--- excesscommands/lavalink/mpause.js
+++ excesscommands/lavalink/mpause.js
@@ -15,7 +15,7 @@ module.exports = {
             .setAuthor({
                 name: lang.songPausedTitle,
                 iconURL: musicIcons.pauseresumeIcon,
-                url: "https://discord.gg/xQF9f9yUEM"
+                url: "https://discord.gg/KNjWAW3SPb"
             })
             .setFooter({ text: lang.footerText, iconURL: musicIcons.footerIcon })
             .setDescription(lang.songPausedDescription);
