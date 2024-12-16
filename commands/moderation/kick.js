9a6af4519759a10acf9359c45d9deacdc8a00520
faccout <faccout41@gmail.com>
2024-12-16 16:29:33 +0300

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -41,7 +41,7 @@ module.exports = {
                 .setAuthor({ 
                     name: lang.kickAlert, 
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/KNjWAW3SPb"
                 })
                 .setDescription(lang.kickOnlySlashCommand)
                 .setTimestamp();
