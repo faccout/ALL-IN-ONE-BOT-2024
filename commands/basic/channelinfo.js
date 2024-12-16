0bef29aacd9187c656367ff0047a4077c4a0cef9
faccout <faccout41@gmail.com>
2024-12-16 16:15:19 +0300

    Update channelinfo.js

--- commands/basic/channelinfo.js
+++ commands/basic/channelinfo.js
@@ -17,7 +17,7 @@ module.exports = {
                 .setAuthor({ 
                     name: lang.channelInfo, 
                     iconURL: "https://cdn.discordapp.com/attachments/1246408947708072027/1256596058683736175/info.png?ex=668157c6&is=66800646&hm=dfa8e8b7b2500f1fc49aaf1de6ee8b9e17a6f824150742ee20e0a8475c63c935&",
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/KNjWAW3SPb"
                 })
                 .setThumbnail(guildIconURL)
                 .setDescription(`
