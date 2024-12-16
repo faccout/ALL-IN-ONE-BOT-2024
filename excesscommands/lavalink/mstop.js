879ca2e717ea2eb7ff631e5bf09613c5183a7e8d
faccout <faccout41@gmail.com>
2024-12-16 16:08:10 +0300

    mstop.js

--- excesscommands/lavalink/mstop.js
+++ excesscommands/lavalink/mstop.js
@@ -17,7 +17,7 @@ module.exports = {
             .setAuthor({
                 name: lang.stoppedTitle,
                 iconURL: musicIcons.stopIcon,
-                url: "https://discord.gg/xQF9f9yUEM"
+                url: "https://discord.gg/KNjWAW3SPb"
             })
             .setFooter({ text: lang.footerText, iconURL: musicIcons.footerIcon })
             .setDescription(lang.queueStoppedText);
