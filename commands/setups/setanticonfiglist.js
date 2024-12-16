a1cb9fb585566ccd2c1bf67a16918723e5da73b9
faccout <faccout41@gmail.com>
2024-12-16 16:33:21 +0300

    Update setanticonfiglist.js

--- commands/setups/setanticonfiglist.js
+++ commands/setups/setanticonfiglist.js
@@ -85,7 +85,7 @@ module.exports = {
         .setAuthor({ 
             name: "Alert!", 
             iconURL: cmdIcons.dotIcon ,
-            url: "https://discord.gg/xQF9f9yUEM"
+            url: "https://discord.gg/KNjWAW3SPb"
         })
         .setDescription('- This command can only be used through slash command!\n- Please use `/setanticonfig`')
         .setTimestamp();
