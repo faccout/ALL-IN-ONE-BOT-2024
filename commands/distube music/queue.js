5b98b9680274a05baace2bcdcf1fbe10f2ffb195
faccout <faccout41@gmail.com>
2024-12-16 16:23:01 +0300

    Update queue.js

--- commands/distube music/queue.js
+++ commands/distube music/queue.js
@@ -22,7 +22,7 @@ module.exports = {
                 .setAuthor({ 
                     name: lang.queueNoSongsTitle, 
                     iconURL: musicIcons.wrongIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/KNjWAW3SPb"
                 })
                 .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })   
                 .setDescription(lang.queueNoSongsMessage);
@@ -39,7 +39,7 @@ module.exports = {
             .setAuthor({ 
                 name: lang.queueTitle, 
                 iconURL: musicIcons.beatsIcon,
-                url: "https://discord.gg/xQF9f9yUEM"
+                url: "https://discord.gg/KNjWAW3SPb"
             })
             .setFooter({ text: 'Distube Player', iconURL: musicIcons.footerIcon })
             .setDescription(`${lang.queueSongs} ${queue.songs.length}`)
