12cee5d9a9a9fe3e50c8f6fa06bc7138141b913b
faccout <faccout41@gmail.com>
2024-12-16 16:31:02 +0300

    Update setnickname.js

--- commands/moderation/setnickname.js
+++ commands/moderation/setnickname.js
@@ -24,7 +24,7 @@ module.exports = {
                 .setAuthor({ 
                     name: "Alert!", 
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/KNjWAW3SPb"
                 })
                 .setDescription(lang.setnicknamePrefixError)
                 .setTimestamp();
